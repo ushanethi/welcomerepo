2851d1a6cc25771c77d1bb0d9ce65aa6763ed14e
usha <ushasreenethi@gmail.com>
2023-07-31 19:06:16 -0500

    multiple bye

--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,9 @@ class FactorialExample{
  public static void main(String args[]){  
   System.out.println("Welcome");
   System.out.println("Bye"); 
+  System.out.println("Bye"); 
+  System.out.println("Bye"); 
+  System.out.println("Bye"); 
  
  }  
 }  
